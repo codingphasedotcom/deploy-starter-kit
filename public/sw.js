f7ab5fbb7a948bf94a76803436d9ab1c62c80bbe
Joe Santos Garcia <joesantosgarcia@gmail.com>
2019-10-25 15:07:36 -0400

    added progressive web app

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 //Delete all caches and keep only one
-const cachNameToKeep = 'codingphase-2-2-19';
+const cachNameToKeep = 'codingphase-10-20-2020';
 
 //Deletion should only occur at the activate event
 self.addEventListener('activate', event => {
@@ -20,23 +20,23 @@ self.addEventListener('activate', event => {
 	); //this line is important in some contexts
 });
 // make sure to uncomment this section below to have your dev starter kit cache everything and becoming a progressive web app
-// self.addEventListener('install', function(e) {
-// 	e.waitUntil(
-// 		caches.open(cachNameToKeep).then(function(cache) {
-// 			return cache.addAll([
-// 				'/',
-// 				'/index.html',
-// 				// '/offline.html',
-// 				'/css/styles.css'
-// 			]);
-// 		})
-// 	);
-// });
+self.addEventListener('install', function(e) {
+	e.waitUntil(
+		caches.open(cachNameToKeep).then(function(cache) {
+			return cache.addAll([
+				'/',
+				'/index.html',
+				// '/offline.html',
+				'/css/styles.css'
+			]);
+		})
+	);
+});
 
-// self.addEventListener('fetch', function(event) {
-// 	event.respondWith(
-// 		caches.match(event.request).then(function(response) {
-// 			return response || fetch(event.request);
-// 		})
-// 	);
-// });
+self.addEventListener('fetch', function(event) {
+	event.respondWith(
+		caches.match(event.request).then(function(response) {
+			return response || fetch(event.request);
+		})
+	);
+});
